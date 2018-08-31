cb8e4924a2bca595232401d45451fd0cd4518446
dokxo <daanmoraal@ittepic.edu.mx>
2018-08-31 14:39:44 -0600

    actualizacion
    se agrego el metodo device.invok. para el funcionamiento del web service en android,con el metodo de check internet

--- AppEvaTec/AppEvaTec/Views/Personas/FicViCpLogin.xaml.cs
+++ AppEvaTec/AppEvaTec/Views/Personas/FicViCpLogin.xaml.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using AppEvaTec.ViewModels.Personas;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Plugin.Connectivity;
 
 namespace AppEvaTec.Views.Personas
 {
@@ -22,9 +25,68 @@ namespace AppEvaTec.Views.Personas
 
             NewUser.Clicked += NewUser_Clicked;
             BtnOc.Clicked += BtnOc_Clicked;
-
+            BtnEntrar.Clicked += BtnEntrar_Clicked;
+            
         }
+        public void CheckInternet(object sender, EventArgs e)
+        {
+            statusInternet.Text = CrossConnectivity.Current.IsConnected ? "Connected" : "Disconnected";
+        }
+        private async void BtnEntrar_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(User.Text))
+            {
+                var j = new modal();
+                await j.DisplayAlert("Error", "El usuario está vacio", "ok");
+                User.Focus();
+                return;
 
+            }
+            if (string.IsNullOrEmpty(Pass.Text))
+            {
+                var j = new modal();
+                await j.DisplayAlert("Error", "La contraseña  está vacia", "ok");
+                Pass.Focus();
+                return;
+            }
+
+            if (statusInternet.Text == "Connected")
+            {
+                if (User.Text == "Admin" && Pass.Text == "admin")
+                    {
+                                     await MainProgress.ProgressTo(1, 2000, Easing.Linear);
+                                     Device.BeginInvokeOnMainThread(async () => {
+
+                                     //string dir ="http: //localhost:60304/api/usuarios?user"+Usuario+ "&password="+Contraseña;
+                                     //HttpClient FicHttpClient = new HttpClient();
+
+                                     //var response = await FicHttpClient.GetStringAsync("http://localhost:60304/api/usuarios?user=DMORAA&password=Dany01");
+                                     try
+                                         {
+                                            //var test = JsonConvert.SerializeObject(response, Formatting.Indented);
+
+                                            await ((NavigationPage)this.Parent).PushAsync(new FicViCpMainPage(null));
+
+                                         }
+                                     catch (Exception ex)
+                                         {
+                                            var j = new modal();
+                                            await j.DisplayAlert("pruebas", "Servidor desactivado", "ok");
+                                         }
+
+                            });
+                    }
+            }
+            else
+            {
+                //metodo local
+                MainProgress.ProgressTo(1, 1000, Easing.SinIn);
+                await ((NavigationPage)this.Parent).PushAsync(new FicViCpMainPage(null));
+            }
+
+           
+            //((NavigationPage)this.Parent).PushAsync(new FicViCpMainPage(null));
+        }
         private void BtnOc_Clicked(object sender, EventArgs e)
         {
             ((NavigationPage)this.Parent).PushAsync(new FicViCpMainPage(null));
@@ -33,6 +95,11 @@ namespace AppEvaTec.Views.Personas
         private void NewUser_Clicked(object sender, EventArgs e)
         {
             ((NavigationPage)this.Parent).PushAsync(new FicViCpRegister(null));
+        }
+
+        private class modal : Page
+        {
+
         }
 
 
